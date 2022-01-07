b9d402f2fe584a3edf4380a8f2b795030c57a9f3
guirkgumbo <rmcguirk109@gmail.com>
2022-01-07 16:22:15 -0500

    Can you believe secrets manager wont work with this shit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 require("@nomiclabs/hardhat-waffle");
 
 // This is a sample Hardhat task. To learn how to create your own go to
@@ -10,12 +12,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.4",
-};
+    networks: {
+      rinkeby: {
+        url: process.env.RINKEBY_URL || "",
+        accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      },
+    },
+    gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+}
